9a3acc6d2602cb1571bb50695f7501bfb9b4f6b2
untilthecore <814898862@qq.com>
2021-09-11 00:05:57 +0800

    freedrawingbed新增《组件懒加载》

--- docs/project/freedrawingbed/sideBar.ts
+++ docs/project/freedrawingbed/sideBar.ts
@@ -6,6 +6,12 @@ const sideBar: SidebarConfigArray = [
     children: [
       '/project/freedrawingbed/README.md',
       '/project/freedrawingbed/初始化.md',
+      {
+        text: '知识点',
+        children: [
+          '/project/freedrawingbed/知识点/组件懒加载.md',
+        ]
+      },
     ]
   }
 ]
