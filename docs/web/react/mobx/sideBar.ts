d8c747291a40bb4f69e35c6e4cc2a32c2399f301
untilthecore <814898862@qq.com>
2021-09-14 23:23:26 +0800

    mobx 新增《数据监听并响应》

--- docs/web/react/mobx/sideBar.ts
+++ docs/web/react/mobx/sideBar.ts
@@ -8,6 +8,7 @@ const sideBar: SidebarConfigArray = [
       "/web/react/mobx/安装和配置.md",
       "/web/react/mobx/mobx初探.md",
       "/web/react/mobx/异步action.md",
+      "/web/react/mobx/数据监听并响应.md",
     ],
   },
 ];
