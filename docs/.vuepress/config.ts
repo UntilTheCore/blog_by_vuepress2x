69100c28e8add0a0317c682534bfb847210595a5
untilthecore <814898862@qq.com>
2021-11-26 14:11:23 +0800

    添加百度和cnzz统计

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,14 +1,22 @@
 import { defineUserConfig } from "vuepress";
 import type { DefaultThemeOptions } from "vuepress";
 import navbar from "./navbar";
-import sideBar from './sidebar'
+import sideBar from "./sidebar";
 
 export default defineUserConfig<DefaultThemeOptions>({
-  lang: 'zh-CN',
+  lang: "zh-CN",
   title: "untilthecore's blog",
   description: "欢迎访问 untilthecore 的个人博客",
   head: [
     ["link", { rel: "icon", href: "/images/favicon.ico" }],
+    ["script", { type: "text/javascript", src: "/js/baidu_statistics.js" }],
+    [
+      "script",
+      {
+        type: "text/javascript",
+        src: "https://v1.cnzz.com/z_stat.php?id=1280635059&web_id=1280635059",
+      },
+    ],
   ],
 
   themeConfig: {
